5b1d5291a9a2a89b9e9555dd29a3f0372de96d50
watare <aurelien.watare@gmail.com>
2019-06-07 02:23:05 +0200

    ajouts des nouvelles bougies au lieu decraser

--- fetchdata_gdax.py
+++ fetchdata_gdax.py
@@ -13,12 +13,18 @@ def fetchdata():
         f.write(data)
         f.close()
 
-    data = public_client.get_product_historic_rates('BTC-EUR', granularity=60,start = "2019-05-15")
+    data = public_client.get_product_historic_rates('BTC-EUR', granularity=60,start = "2019-06-01")
+    #print(data)
     listdata = []
     for da in data:
         da[0] = da[0] - da[0]%60
         listdata.append({'time' :da[0], 'open' :da[1],'high':da[2],'low':da[3],'close':da[4],'volume':da[5]})
-
+    with open(homedir +'/server_crypto/data/data_received_tlohcv.json', 'r') as fold:
+        data_dict = json.load(fold)
+        listdata = listdata + data_dict
+        listdata = [i for n, i in enumerate(listdata) if i not in listdata[n + 1:]]
+        print (listdata)
+        fold.close()
     with open(homedir +'/server_crypto/data/data_received_tlohcv.json', 'w') as f:
         json.dump(listdata,f)
         f.close()
